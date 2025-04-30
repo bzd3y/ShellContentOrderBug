40cf129794e804d6bc52aaa6d1c9c55e805bae2f
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2025-04-30 18:51:55 -0400

    Add project files.

---
+++ ShellContentOrderBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShellContentBugs
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ShellContentOrderBug/OtherPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShellContentBugs;
+
+public partial class OtherPage : ContentPage
+{
+	public OtherPage()
+	{
+		InitializeComponent();
+	}
+}
